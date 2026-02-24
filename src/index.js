84610b561346fddc8a783921a1f32ce9b95b3bf5
Vidhyadhran-03 <rvdharan31@gmail.com>
2026-02-24 20:57:46 +0530

    feat: overhaul system architecture
    BREAKING CHANGE: this update migrates the database schema and is not backward compatible

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ console.log("Integrated UPI payments too");
 console.log("Integrating another payment gateway to enhance the flow");
 
 console.log("Trying Major update here");
+
+console.log("Testing Major Changes");
